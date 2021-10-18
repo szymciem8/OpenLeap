2e6ea94c94a23f487ae1f8688c7f93da42eeb514
szymon <szymciem8@gmail.com>
2021-10-18 22:06:41 +0200

    new update

--- main.py
+++ main.py
@@ -21,6 +21,10 @@ DIMENSIONS = [WIDTH, HEIGHT]
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
+background = np.zeros([HEIGHT,WIDTH,3], dtype=np.uint8)
+background.fill(0)
+
+
 #Dataclass that describes position and other vales of each hand
 @dataclass
 class Data:
@@ -90,7 +94,7 @@ def left_or_right(index, hand, results, mode='AI'):
 
 
 #Get position of given hand landmark, such as wrist or tip on an index finger
-def get_position(index=0, landmark_idx = mp_hands.HandLandmark.INDEX_FINGER_TIP, dim=None):
+def get_position(results, index=0, landmark_idx = mp_hands.HandLandmark.INDEX_FINGER_TIP, dim=None):
 
     """
     Finds normalized position or of given hand landmark. Additionally, it can calculate the position on the screen
@@ -117,7 +121,7 @@ def get_position(index=0, landmark_idx = mp_hands.HandLandmark.INDEX_FINGER_TIP,
     return x, y, z
 
 #Calculate distance between two different hand landmarks
-def get_distance_bettween_landmarks(index, landmark_1, landmark_2, dim):
+def get_distance_bettween_landmarks(results, index, landmark_1, landmark_2, dim):
 
     """
     Calculates distance between two given hand landmarks. 
@@ -130,8 +134,8 @@ def get_distance_bettween_landmarks(index, landmark_1, landmark_2, dim):
     returns: float
     """
 
-    x1, y1, z1 = get_position(0, landmark_1, dim)
-    x2, y2, z2 = get_position(0, landmark_2, dim)
+    x1, y1, z1 = get_position(results, 0, landmark_1, dim)
+    x2, y2, z2 = get_position(results, 0, landmark_2, dim)
 
     x1 *= dim[0]
     x2 *= dim[0]
@@ -143,10 +147,16 @@ def get_distance_bettween_landmarks(index, landmark_1, landmark_2, dim):
 
     return distance
 
-def get_angle(index, landmark_idx, mode='half'):
+def get_angle(results, index, landmark_idx, mode='half', unit='radians'):
     """
     Calculates angle using atan2 with wrist as a base. 
+
+    parameters:
+
+    returns:
     """
+
+    angle=0
 
     wrist_x = results.multi_hand_landmarks[index].landmark[mp_hands.HandLandmark.WRIST].x
     wrist_y = results.multi_hand_landmarks[index].landmark[mp_hands.HandLandmark.WRIST].y
@@ -159,97 +169,97 @@ def get_angle(index, landmark_idx, mode='half'):
 
     angle = math.atan2(realitive_y, realitive_x)
 
-    print(angle)
+    if mode=='half' and angle>0: angle=0
 
-hand_type = None
-with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands:
-    while cap.isOpened():
-        ret, frame = cap.read()
+    if unit=='degree':
+        angle = 180*abs(angle)/math.pi
 
-        #BGR to RGB
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    return angle
 
-        #Flip horizontal
-        image = cv2.flip(image, 1)
+def show_data():
+    """
+    Shows collected and calculated data in opencv window. 
+    """
+    pass
 
-        #Set flag
-        image.flags.writeable = False
+def main():
 
-        #Detections
-        results = hands.process(image)
+    """
+    Main function that runs the core of the program. 
+    """
 
-        #Set flag back to True
-        image.flags.writeable = True
+    hand_type = None
+    with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands:
+        while cap.isOpened():
+            ret, frame = cap.read()
 
-        #RGB to BGR
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            #BGR to RGB
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            #Flip horizontal
+            image = cv2.flip(image, 1)
+
+            #Set flag
+            image.flags.writeable = False
+
+            #Detections
+            results = hands.process(image)
+
+            #Set flag back to True
+            image.flags.writeable = True
+
+            #RGB to BGR
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            #Rendering results
+            if results.multi_hand_landmarks:
+                n_hands = len(results.multi_hand_landmarks)
+                for index, hand in enumerate(results.multi_hand_landmarks):
+                    mp_drawing.draw_landmarks(background, hand, mp_hands.HAND_CONNECTIONS,
+                                            mp_drawing.DrawingSpec(color=(0,255,0), thickness=2, circle_radius=4),
+                                            mp_drawing.DrawingSpec(color=(0,0,255), thickness=2, circle_radius=4))
+
+                    
+                    
+                    if n_hands > 0:
+                        #If there are two hands
+                        if left_or_right(index, hand, results, 'position'):
+                            hand_type, coord = left_or_right(index, hand, results, 'position')
+                            cv2.putText(background, hand_type, coord, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+                            x, y, z = get_position(results, index, mp_hands.HandLandmark.WRIST, DIMENSIONS)
+
+                            data[hand_type].x = x
+                            data[hand_type].y = y
+                            data[hand_type].z = z
+                            data[hand_type].distance = get_distance_bettween_landmarks(
+                                                                                        results,
+                                                                                        index, 
+                                                                                        mp_hands.HandLandmark.INDEX_FINGER_TIP, 
+                                                                                        mp_hands.HandLandmark.THUMB_TIP, 
+                                                                                        DIMENSIONS
+                                                                                        )
+                                                                                        
+                            data[hand_type].angle = get_angle(results, index, mp_hands.HandLandmark.MIDDLE_FINGER_MCP, unit='degree')
+
+                            cv2.putText(image, hand_type, (x,y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+
+                            x1, y1, z1 = get_position(results, index, mp_hands.HandLandmark.INDEX_FINGER_TIP, DIMENSIONS)
+                            x2, y2, z2 = get_position(results, index, mp_hands.HandLandmark.THUMB_TIP, DIMENSIONS)
+                            
+                            #Draw line that connects index finger tip and thumb tip
+                            cv2.line(background, (x1, y1), (x2, y2), (255, 125, 100), 2)
+
+                    print(data)
+
+            cv2.imshow("Hand Tracking", background)
+            background.fill(0)
+
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
 
 
-        #Rendering results
-        if results.multi_hand_landmarks:
-            n_hands = len(results.multi_hand_landmarks)
-            for index, hand in enumerate(results.multi_hand_landmarks):
-                mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS,
-                                         mp_drawing.DrawingSpec(color=(0,255,0), thickness=2, circle_radius=4),
-                                         mp_drawing.DrawingSpec(color=(0,0,255), thickness=2, circle_radius=4))
-
-                dist = get_distance_bettween_landmarks(index, mp_hands.HandLandmark.INDEX_FINGER_TIP, mp_hands.HandLandmark.THUMB_TIP, DIMENSIONS)
-
-
-                get_angle(0, mp_hands.HandLandmark.MIDDLE_FINGER_MCP)
-                if n_hands > 0:
-                    #If there are two hands
-                    if left_or_right(index, hand, results, 'position'):
-                        hand_type, coord = left_or_right(index, hand, results, 'position')
-                        cv2.putText(image, hand_type, coord, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
-                        x, y, z = get_position(index, mp_hands.HandLandmark.WRIST, DIMENSIONS)
-
-
-                        data[hand_type].x = x
-                        data[hand_type].y = y
-                        data[hand_type].z = z
-                        data[hand_type].angle = 0
-                        data[hand_type].distance = dist
-
-                        # print(data)
-
-                    else:
-                        data['right'] = Data(0, 0, 0, 0, 0)
-
-                    if hand_type == "right":
-                        x1, y1, z1 = get_position(index, mp_hands.HandLandmark.INDEX_FINGER_TIP, DIMENSIONS)
-                        #cv2.putText(image, 'INDEX', (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
-
-                        x2, y2, z2 = get_position(index, mp_hands.HandLandmark.THUMB_TIP, DIMENSIONS)
-                        #cv2.putText(image, 'THUMB', (x2, y2), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
-
-                        #Draw line that connects index finger tip and thumb tip
-                        cv2.line(image, (x1, y1), (x2, y2), (255, 125, 100), 2)
-
-                        #Calculate the distance between those two points
-                        #cv2.putText(image, str(dist), (x2, y2), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
-                        #cv2.putText(image, str(z1), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
-
-                else:
-                        x1, y1, z1 = get_position(index, mp_hands.HandLandmark.INDEX_FINGER_TIP, DIMENSIONS)
-                        #cv2.putText(image, 'INDEX', (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
-
-                        x2, y2, z2 = get_position(index, mp_hands.HandLandmark.THUMB_TIP, DIMENSIONS)
-                        #cv2.putText(image, 'THUMB', (x2, y2), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
-
-                        #Draw line that connects index finger tip and thumb tip
-                        cv2.line(image, (x1, y1), (x2, y2), (255, 125, 100), 2)
-
-                        #Calculate the distance between those two points
-                        cv2.putText(image, str(dist), (x2, y2), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
-
-                #print(data)
-
-        #image = cv2.flip(image, 0)
-        cv2.imshow("Hand Tracking", image)
-
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
+if __name__=='__main__':
+    main()
--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print(not 0)
+import cv2
+import numpy as np
+
+img = np.zeros([500,500,3], dtype=np.uint8)
+
+img.fill(0)
+
+cv2.imshow('test', img)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
